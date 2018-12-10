bb756d5f58ee5e76330d498058dfb17f5bb27d52
tosh7 <zlia.6.lj.425@gmail.com>
2018-12-11 01:01:04 +0900

    first init

---
+++ Voice-ChangingBowtie/Model/AudioChageModel.swift
@@ -0,0 +1,14 @@
+//
+//  AudioChageModel.swift
+//  Voice-ChangingBowtie
+//
+//  Created by Satoshi Komatsu on 2018/12/11.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+final class AudioChangeModel {
+    init{}
+}
--- Voice-ChangingBowtie/ViewController/ViewController.swift
+++ Voice-ChangingBowtie/ViewController/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
+    @IBAction func input(_ sender: Any) {
+    }
+    
+    @IBAction func output(_ sender: Any) {
+    }
 }
 
