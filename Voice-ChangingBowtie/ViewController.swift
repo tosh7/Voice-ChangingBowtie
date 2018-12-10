daf7eea5628a3c83d4563bac2df931c46a96974f
tosh7 <zlia.6.lj.425@gmail.com>
2018-12-11 00:26:51 +0900

    Initial Commit

---
+++ Voice-ChangingBowtie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice-ChangingBowtie
+//
+//  Created by Satoshi Komatsu on 2018/12/11.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
