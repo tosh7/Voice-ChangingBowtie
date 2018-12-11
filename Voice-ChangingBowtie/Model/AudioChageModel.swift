a79f9bb20dd6715d868d6795d7bb557cdebf3c3d
tosh7 <zlia.6.lj.425@gmail.com>
2018-12-11 13:59:36 +0900

    made recording function

--- Voice-ChangingBowtie/Model/AudioChageModel.swift
+++ Voice-ChangingBowtie/Model/AudioChageModel.swift
@@ -10,5 +10,5 @@ import Foundation
 import AVFoundation
 
 final class AudioChangeModel {
-    init{}
+    init() {}
 }
--- Voice-ChangingBowtie/ViewController/ViewController.swift
+++ Voice-ChangingBowtie/ViewController/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class ViewController: UIViewController {
+    
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    var isRecording = false
+    var isPlaying = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpAudioRecorder()
     }
 
-
-    @IBAction func input(_ sender: Any) {
+    @IBAction func record(_ sender: Any) {
+        if !isRecording {
+            isRecording = true
+            audioRecorder.record()
+        } else {
+            isRecording = false
+            audioRecorder.stop()
+        }
     }
     
-    @IBAction func output(_ sender: Any) {
+    @IBAction func play(_ sender: Any) {
     }
 }
 
+extension ViewController: AVAudioRecorderDelegate {
+    func setUpAudioRecorder() {
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(.playAndRecord, mode: .default)
+        try! session.setActive(true)
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 44100,
+            AVNumberOfChannelsKey: 2,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        audioRecorder = try! AVAudioRecorder(url: getAudioFilrUrl(), settings: settings)
+        audioRecorder.delegate = self
+    }
+}
+
+extension ViewController {
+    func getAudioFilrUrl() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docsDirect = paths[0]
+        let audioUrl = docsDirect.appendingPathComponent("recording.m4a")
+        
+        return audioUrl
+    }
+}
