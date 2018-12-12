934ec892f543fea894248ec6fb00f28c426b0b8e
tosh7 <zlia.6.lj.425@gmail.com>
2018-12-12 10:16:03 +0900

    add UI parts

--- Voice-ChangingBowtie/ViewController/ViewController.swift
+++ Voice-ChangingBowtie/ViewController/ViewController.swift
@@ -15,6 +15,33 @@ final class ViewController: UIViewController {
     var isRecording = false
     var isPlaying = false
     
+    @IBOutlet weak var speedSlider: UISlider! {
+        didSet {
+            speedSlider.minimumValue = 0
+            speedSlider.maximumValue = 2
+            speedSlider.setValue(1, animated: true)
+            speedSlider.tintColor = UIColor.green
+        }
+    }
+    @IBOutlet weak var pitchSlider: UISlider! {
+        didSet {
+            pitchSlider.minimumValue = -1000
+            pitchSlider.maximumValue = 1000
+            pitchSlider.setValue(0, animated: true)
+            pitchSlider.tintColor = UIColor.green
+        }
+    }
+    @IBOutlet weak var echoSwitch: UISwitch! {
+        didSet {
+            echoSwitch.isOn = false
+        }
+    }
+    @IBOutlet weak var reverbSwitch: UISwitch! {
+        didSet {
+            reverbSwitch.isOn = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,7 +61,10 @@ final class ViewController: UIViewController {
     @IBAction func play(_ sender: Any) {
         if !isPlaying {
             isPlaying = true
-            audio.playSound()
+            audio.playSound(speed: speedSlider.value,
+                            pitch: pitchSlider.value,
+                            echo: echoSwitch.isOn,
+                            reverb: reverbSwitch.isOn)
         } else {
             isPlaying = false
             audio.audioPlayer.stop()
